98ae2c290e4c24a8982874d57c5d82f1af19b7b9
OnePaPerHoon <wkdgns720@naver.com>
2025-04-08 17:15:02 +0900

    fix

--- index.js
+++ index.js
@@ -2,19 +2,12 @@ import { writeFileSync } from 'node:fs';
 import Parser from "rss-parser";
 
 /**
- * README.MD에 작성될 페이지 텍스트
- * @type {string}
+ * README.MD
  */
+
 let text = `# Hi there 👋
 
 ## 이런 환경에 익숙해요✍🏼
-
-## 언어
-
-<p>
-  <img alt="" src= "https://img.shields.io/badge/JavaScript-F7DF1E?style=flat-square&logo=JavaScript&logoColor=white"/>
-  <img alt="" src= "https://img.shields.io/badge/TypeScript-black?logo=typescript&logoColor=blue"/>
-</p>
 
 ## 📕 Latest Blog Posts
 
@@ -29,24 +22,21 @@ const parser = new Parser({
 (async () => {
 
     // 피드 목록
-    const feed = await parser.parseURL('https://onepaperhoon.tistory.com/rss'); // 본인의 블로그 주소
+    const feed = await parser.parseURL('https://systorage.tistory.com/rss');
 
-    text += `<ul>`;
-
-    // 최신 10개의 글의 제목과 링크를 가져온 후 text에 추가
-    for (let i = 0; i < 10; i++) {
+    // 최신 5개의 글의 제목과 링크를 가져온 후 text에 추가
+    for (let i = 0; i < 5; i++) {
         const {title, link} = feed.items[i];
         console.log(`${i + 1}번째 게시물`);
         console.log(`추가될 제목: ${title}`);
         console.log(`추가될 링크: ${link}`);
-        text += `<li><a href='${link}' target='_blank'>${title}</a></li>`;
+        text += `<a href=${link}>${title}</a></br>`;
     }
 
-    text += `</ul>`;
-
-    // README.md 파일 생성
+    // README.md 파일 작성
     writeFileSync('README.md', text, 'utf8', (e) => {
-        console.log(e);
+        console.log(e)
     })
-    console.log('업데이트 완료');
+
+    console.log('업데이트 완료')
 })();
